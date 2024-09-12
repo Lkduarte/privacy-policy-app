00cfc6d4287732096091bce0e8db7f6aa29777ad
Lucas Rodrigo <Lucas.moreira34@fatec.sp.gov.br>
2024-09-12 11:47:29 -0300

    Modify archives

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Register from './Register';
 import UserDashboard from './UserDashboard';
 import ConsentHistory from './ConsentHistory';
 import Notification from './Notification';
+import { Routes } from 'react-router-dom/dist';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Register} />
           <Route path="/dashboard" component={UserDashboard} />
           <Route path="/consent-history" component={ConsentHistory} />
           <Route path="/notifications" component={Notification} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
